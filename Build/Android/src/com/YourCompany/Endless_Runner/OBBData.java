ed016c06246806dbcf98535d2d68bc8ecf9d0fb8
draco28 <45367879+draco28@users.noreply.github.com>
2019-04-26 00:26:06 +0530

    dd

--- Build/Android/src/com/YourCompany/Endless_Runner/OBBData.java
+++ Build/Android/src/com/YourCompany/Endless_Runner/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-200669371L // the length of the file in bytes
+297235442L // the length of the file in bytes
 )
 };
 };
